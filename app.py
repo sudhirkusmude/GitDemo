c2e708d87c87717ea181980e73bd627a5fea57a7
sudhirkusmude <sudhirkusmude1993@gmai.com>
2024-03-06 10:32:58 +0530

    prime def

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # Hi hello how are you
+
+def primary_num(num):
+    for i in range(2,int(num/2)):
+        if num%2==0:
+            print("not prime")
+            break
+    else:
+        print("prime")
+
+primary_num(7)
+
