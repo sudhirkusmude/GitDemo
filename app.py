2c4a0fec393916337d303c24a903643800e40fe1
sudhirkusmude <sudhirkusmude1993@gmai.com>
2024-03-11 18:16:17 +0530

    app

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # Hi hello how are you
 
-def primary_num(num):
-    for i in range(2,int(num/2)):
-        if num%2==0:
-            print("not prime")
-            break
-    else:
-        print("prime")
-
-primary_num(7)
+def primary_num():
+    prime_n=[]
+    for num in range(101):
+        
+        for i in range(2,int(num/2)):
+            if num%2==0:
+                # print("not prime")
+                break
+        else:
+            # print("prime")
+            prime_n.append(num)
+    print(prime_n)
+primary_num()
 
